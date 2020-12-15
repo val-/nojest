76ade790dd4ddef85c20fce2b2999db8e0d9b0cb
val- <webkoder@ya.ru>
2020-12-15 15:55:35 +0300

    Router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,69 @@
 import 'fontsource-roboto';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/create-order">Create order</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/create-order">
+              <CreateOrder />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+}
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  );
+}
+
+function CreateOrder() {
+  return (
+    <div>
+      <h2>Create order</h2>
+    </div>
   );
 }
 
